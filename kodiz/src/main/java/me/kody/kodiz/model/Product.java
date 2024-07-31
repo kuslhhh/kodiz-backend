809def2509d1cb7d00f476a65de84ab7c0b34c0f
kuslhhh <kushaljadhav7745@gmail.com>
2024-07-31 17:08:42 +0530

    classes

--- kodiz/src/main/java/me/kody/kodiz/model/Product.java
+++ kodiz/src/main/java/me/kody/kodiz/model/Product.java
@@ -3,35 +3,38 @@ package me.kody.kodiz.model;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import org.springframework.data.annotation.Id;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private String desc;
+    private String description;
     private String brand;
     private BigDecimal price;
     private String category;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="dd-mm-yyyy")
     private Date releaseDate;
-    private boolean available;
-    private int quantity;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageDate;
+
+
 }
--- kodiz/src/main/java/me/kody/kodiz/service/ProductService.java
+++ kodiz/src/main/java/me/kody/kodiz/service/ProductService.java
@@ -1,9 +1,11 @@
 package me.kody.kodiz.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import me.kody.kodiz.model.Product;
 import me.kody.kodiz.repo.ProductRepo;
@@ -14,12 +16,35 @@ public class ProductService {
     @Autowired
     private ProductRepo repo;
 
-    public List<Product> getAllProduct() {
+    public List<Product> getAllProducts() {
+
         return repo.findAll();
+
     }
 
-    public Product getAllProductById(int id) {
-    
-        return repo.findById(id).get();
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageDate(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return repo.searchProducts(keyword);
     }
 }
